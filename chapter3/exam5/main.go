3c5642d9fa90486978729348c0cb6b6fba016bca
Takuya Tokuda <cs.toku.mail@gmail.com>
2020-02-28 01:27:20 +0900

    add chapter3/exam5

--- chapter3/exam5/main.go
+++ chapter3/exam5/main.go
@@ -3,12 +3,42 @@ package main
 import (
     "io"
     "fmt"
+    "errors"
+    "strings"
+    "bytes"
 )
 
-func CopyN(dest io.Writer, src io.Reader, length int) {
-    
+func CopyN(dst io.Writer, src io.Reader, n int) (int, error) {
+    BUFSIZE := 64
+    buf := make([]byte, BUFSIZE)
+    var written int
+
+    if dst == nil || src == nil {
+        return 0, errors.New("src or dst is nil...")
+    }
+
+    for {
+        cnt, err := src.Read(buf)
+        switch {
+            case err == io.EOF:
+                return written, nil
+            case err != nil:
+                return 0, err
+        }
+
+        if cnt < n {
+            n -= cnt
+        } else {
+            cnt = n
+        }
+        w, err := dst.Write(buf[:cnt])
+        written += w
+    }
 }
 
 func main() {
-    fmt.Println("hoge")
+    s := strings.NewReader("hogehoge")
+    var b bytes.Buffer
+    n, _ := CopyN(&b, s, 5)
+    fmt.Printf("%v: %v\n", n, b.String())
 }
